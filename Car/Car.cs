359eff5e17427b8715721e82f8e17aa4bd7c03d6
Joel Ringh <joel.ringh@gmail.com>
2025-09-18 10:20:31 +0200

    Add project files.

---
+++ Car/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    internal class Car
+    {
+        public string brand;
+        public string color;
+        public int year;
+        public int horsePower;
+        public int gears;
+        public bool engineOn;
+
+
+
+    }
+}
---
+++ Car/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Car
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+            myCar.brand = "Jeep";
+            myCar.color = "Red";
+            myCar.year = 2020;
+            myCar.horsePower = 300;
+            myCar.gears = 8;
+            myCar.engineOn = false;
+
+            Console.WriteLine($"Brand: {myCar.brand} Color: {myCar.color} Year: {myCar.year} Engine: {myCar.engineOn}");
+        }
+    }
+
+    
+}
